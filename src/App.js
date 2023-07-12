26fb66162a39fd9b8808f4a87c7c052bb0bfc4dd
constantly-dev <rntqhdl10@naver.com>
2023-07-12 13:22:35 +0900

    feat: 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './Styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from './Styles/Theme';
+// import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <div className="App"></div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Styles/Theme.js
@@ -0,0 +1,8 @@
+export const Theme = {
+  colors: {
+    WHITE: '#FFFFFF',
+    SKYBLUE: '#2A9DF4',
+    BLUE1: '#1167B1',
+    BLUE2: '#03254C',
+  },
+};
